7fd6fdbbe898241e06f7ef026d1b35b8088ae85e
sagarsangwan <iamsagarsangwan@gmail.com>
2022-03-31 02:47:56 +0530

    root route added

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('sum/<number1>/<number2>', views.sum, name="sum"),
     path('difference/<number1>/<number2>', views.difference, name="difference"),
     path('multiplication/<number1>/<number2>',
--- app/views.py
+++ app/views.py
@@ -1,4 +1,14 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+
+
+def home(request):
+    number1 = 1
+    number2 = 2
+    return HttpResponse(
+        '<a href="/api/sum/{}/{}">Sum</a><br>'
+        '<a href="/api/difference/{}/{}">Difference</a><br>'
+        '<a href="/api/multiplication/{}/{}">Multiplication</a><br>'
+        '<a href="/api/division/{}/{}">Division</a>'.format(number1, number2, number1, number2, number1, number2, number1, number2))
 
 
 def sum(request, number1, number2):
